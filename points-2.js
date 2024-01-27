a5a989d1dd18a0a572e0325425e8c04db873bd20
iuansonleung <137747619+iuansonleung@users.noreply.github.com>
2024-01-27 22:27:59 +0800

    Update points-2.js

--- points-2.js
+++ points-2.js
@@ -9,9 +9,16 @@ var pointJson = {
       },
       "properties": {
         "name": "Hong Kong Wetland Park",
-        "url": "https://ecodesignliteracy.hku.hk/hong-kong-wetland-park",
-        "media": "https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-HKWP.jpg?raw=true"
-      }
-    }
+        "description": "<a href=\"http://hku.hk\" target=\"_parent\">Read more</a>",
+        "styleUrl": "#icon-1899-0288D1",
+        "icon-opacity": 1,
+        "icon-color": "#0288d1",
+        "icon-scale": 1,
+        "icon-offset": [32, 64],
+        "icon-offset-units": ["pixels", "insetPixels"],
+        "icon": "https://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png",
+        "media": "https://raw.githubusercontent.com/iuansonleung/CEP/main/Downsampled/ThumbnailsHong%20Kong%20Wetland%20Park-C.jpg"
+      },
+    },
   ]
-}
+},
