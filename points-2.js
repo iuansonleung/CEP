ddf18dd96348c2233cc874471adaad69acab860d
iuansonleung <137747619+iuansonleung@users.noreply.github.com>
2024-01-27 22:22:34 +0800

    Update points-2.js

--- points-2.js
+++ points-2.js
@@ -1,1 +1,17 @@
-var pointJson = {"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[114.0065831,22.4700585,0]},"properties":{"name":"Hong Kong Wetland Park","url":”https://ecodesignliteracy.hku.hk/hong-kong-wetland-park”, “media":" https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-HKWP.jpg?raw=true"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[113.9640511,22.3043674,0]},"properties":{"name":"Tung Chung New Town Extension (East) Eco-shoreline,"url": “https://ecodesignliteracy.hku.hk/tung-chung-ecoshoreline”,"media":" https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-TCE.jpg?raw=true"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[114.115245,22.494667,0]},"properties":{"name":"Fanling Golf Course,"url":"https://ecodesignliteracy.hku.hk/fanling-golf-course”,”media":" https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-FL.jpg?raw=true"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[114.051804,22.451433,0]},"properties":{"name":"Sha Po Marsh in Park Yoho","url":”” https://ecodesignliteracy.hku.hk/park-yoho”,"media":" https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-YH.jpg?raw=true"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[113.9566385,22.5282388,0]},"properties":{"name":"Dasha Ecological Corridor","url":” https://ecodesignliteracy.hku.hk/dasha-ecological-corridor”,"media":" https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-DS.jpg?raw=true"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[114.42,30.55152,0]},"properties":{"name":"Wuhan East Lake Green Corridor","url":" https://ecodesignliteracy.hku.hk/wuhan-east-lake-green-corridor ","media":" https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-WH.jpg?raw=true"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[114.0338268,22.5098265,0]},"properties":{"name":"IBC 2017 Memorial Garden","url":" https://ecodesignliteracy.hku.hk/ibc-memorial-garden ","media":" https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-IBC.jpg?raw=true"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[114.1129771,22.5087952,0]},"properties":{"name":"Ho Sheung Heung”,"url":" https://ecodesignliteracy.hku.hk/ho-sheung-heung ","media":" https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-HSH.jpg?raw=true"}},{"type":"Feature","geometry":{"type":"Point","coordinates":[114.1415542, 22.5245424,0]},"properties":{"name":" Hung Lung Hang Forest Plot”,"url" https://ecodesignliteracy.hku.hk/hung-lung-hang:”,"media":" https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-HLH.jpg?raw=true"}}]};
+var pointJson = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [114.0065831, 22.4700585, 0]
+      },
+      "properties": {
+        "name": "Hong Kong Wetland Park",
+        "url": "https://ecodesignliteracy.hku.hk/hong-kong-wetland-park",
+        "media": "https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-HKWP.jpg?raw=true"
+      }
+    }
+  ]
+},
