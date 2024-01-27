f195ed8d64393851a7b693e806510b2154a528a5
iuansonleung <137747619+iuansonleung@users.noreply.github.com>
2024-01-27 22:25:56 +0800

    Update points-2.js

--- points-2.js
+++ points-2.js
@@ -11,7 +11,7 @@ var pointJson = {
         "name": "Hong Kong Wetland Park",
         "url": "https://ecodesignliteracy.hku.hk/hong-kong-wetland-park",
         "media": "https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-HKWP.jpg?raw=true"
-      },
-    },
-  ],
-},
+      }
+    }
+  ]
+}
